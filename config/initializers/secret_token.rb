0dd7083493b75314abf1e4108a1de0b28efe0d2d
Casper <nikt.net>
2013-04-24 23:45:30 +1200

    Refinery Empty

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Test2::Application.config.secret_token = '207f1ecba987b6662475215eea8bc8d618179f129e4567aecd2e9ff65bb60975c52e20e04da185d9c2a9162cf8c6fa8f8c43bff852a7c6d4710f86db1197cfe6'
